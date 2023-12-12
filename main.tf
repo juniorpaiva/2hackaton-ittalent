a5b02a5a8cc62d42a7899645c5a3633ad67b1460
juniorpaiva <edvaldojuniorinfo@gmail.com>
2023-12-11 21:46:39 -0300

    correção 1.2

--- main.tf
+++ main.tf
@@ -28,20 +28,20 @@ resource "random_pet" "sg" {}
 data "aws_ami" "ubuntu" {
   most_recent = true
 
-  owners = ["099720109477"]
+filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
 
-  filter {
-    name = "ubuntu"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20230919"]
-  }  
- 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+
+  owners = ["099720109477"]
+}
 #### ADICIONE AQUI AS CONFIGURAÇÕES DO BLOCO DATA PARA CAPTURAR O AMI DO UBUNTU NA AWS #####
 
-}
 
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
